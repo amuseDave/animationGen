cb31235485d1884893bc298f4f7ea79e72f16c98
amuseDave <amuseddavid@gmail.com>
2024-12-05 15:40:30 +0200

    s

--- src/components/Controller/Custom/NotDD/Controls/Opacity.jsx
+++ src/components/Controller/Custom/NotDD/Controls/Opacity.jsx
@@ -1,9 +1,11 @@
 import { useSelector } from "react-redux";
 import eye from "../../../../../assets/svgs/eye.svg";
 import { handleValueInputs } from "../../../../../utils/helper";
+import { useRef } from "react";
 
 export default function Opacity({ handleStyle }) {
   const activeKeyFrame = useSelector((state) => state.custom.activeKeyFrame);
+  const inputEl = useRef();
 
   const opacity = useSelector(
     (state) => state.custom.keyFrames[activeKeyFrame].opacity
@@ -14,22 +16,29 @@ export default function Opacity({ handleStyle }) {
       <p>Opacity</p>
       <div className="control-input-container">
         <div
-          style={{ opacity: opacity / 100 }}
-          className="bg-white control-square"
-        ></div>
-        <input
-          className="control-value-input control-main-color"
-          style={{ width: `${`${opacity}`.length * 8 + 5}px` }}
-          value={opacity}
-          onChange={(e) => {
-            const value = handleValueInputs(e.target.value, 100);
-            console.log(value);
-
-            if (isNaN(value)) return;
-            handleStyle({ target: { value } }, "set-opacity");
+          onClick={() => {
+            inputEl.current.focus();
           }}
-        />
-        <p className="control-main-color">%</p>
+          className="flex items-center w-full"
+        >
+          <div
+            style={{ opacity: opacity / 100 }}
+            className="bg-white control-square"
+          ></div>
+          <input
+            ref={inputEl}
+            className="control-value-input control-main-color"
+            style={{ width: `${`${opacity}`.length * 8 + 5}px` }}
+            value={opacity}
+            onChange={(e) => {
+              const value = handleValueInputs(e.target.value, 100);
+              console.log(value);
+              if (isNaN(value)) return;
+              handleStyle({ target: { value } }, "set-opacity");
+            }}
+          />
+          <p className="control-main-color">%</p>
+        </div>
 
         <img src={eye} className="ml-auto mr-1" />
       </div>
--- src/components/Controller/Custom/NotDD/Controls/Rotate.jsx
+++ src/components/Controller/Custom/NotDD/Controls/Rotate.jsx
@@ -2,8 +2,10 @@ import { useSelector } from "react-redux";
 import rotateLeft from "../../../../../assets/svgs/rotate-left .svg";
 import rotateSvg from "../../../../../assets/svgs/rotate.svg";
 import { handleTranslateInputs } from "../../../../../utils/helper";
+import { useRef } from "react";
 
 export default function Rotate({ handleStyle }) {
+  const inputEl = useRef();
   const activeKeyFrame = useSelector((state) => state.custom.activeKeyFrame);
   const rotate = useSelector(
     (state) => state.custom.keyFrames[activeKeyFrame].rotate
@@ -14,10 +16,15 @@ export default function Rotate({ handleStyle }) {
       <p className="">Rotate</p>
 
       <div className="control-input-container control-main-color">
-        <div className="flex items-center">
+        <div
+          onClick={() => {
+            inputEl.current.focus();
+          }}
+          className="flex items-center w-full"
+        >
           <img src={rotateLeft} className="mx-1" />
-
           <input
+            ref={inputEl}
             className="control-value-input"
             onChange={(e) => {
               const val = handleTranslateInputs(e.target.value.trim(), 980);
@@ -28,10 +35,11 @@ export default function Rotate({ handleStyle }) {
               handleStyle({ target: { value: val } }, "set-rotate");
             }}
             style={{
-              width: `${`${rotate}`.length * 8 + 7}px`,
+              width: `${`${rotate}`.length * 10 + 2}px`,
             }}
             value={rotate}
           />
+          °
         </div>
 
         <img src={rotateSvg} className="w-5 h-5 ml-auto mr-1" />
